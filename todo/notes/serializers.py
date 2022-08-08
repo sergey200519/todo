dfb56936a978cae6235a143a88be2bb0a62e1d01
sergey <oborok05@bk.ru>
2022-08-08 16:22:35 +0300

    lesson1

---
+++ todo/notes/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+from .models import Users
+
+
+class UsersSerializer(ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ("username", "first_name", "last_name", "email")
