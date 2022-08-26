f820a541f1ed644441b0228dbf887527d430038e
sergey <oborok05@bk.ru>
2022-08-26 18:53:03 +0300

    lesson7-commit

--- todo/authapp/views.py
+++ todo/authapp/views.py
@@ -6,14 +6,22 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.viewsets import ModelViewSet, ViewSet, GenericViewSet
+from rest_framework.permissions import AllowAny, BasePermission
 
 from .models import User
 from .serializers import UsersSerializer
 
 
+class SuperUserOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+
 class UsersModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UsersSerializer
+    permission_classes = [SuperUserOnly]
 # class UsersModelViewSet(ListModelMixin, DestroyModelMixin, RetrieveAPIView, UpdateAPIView, GenericViewSet):
 #     queryset = User.objects.all()
 #     serializer_class = UsersSerializer
--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -6,74 +6,113 @@ import ProjectList from "./components/Project"
 import TodoList from "./components/Todo"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import LoginForm from "./components/Auth";
 import NotFound404 from "./components/NotFound404";
-import {BrowserRouter, Route, Routes, Link, Navigate, useLocation} from 'react-router-dom'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  Navigate,
+  useLocation
+} from 'react-router-dom'
 import axios from "axios";
+import Cookies from "universal-cookie";
 
-class App extends  React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      'users': [],
-      "projects": [],
-      "todo": []
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        'users': [],
+        "projects": [],
+        "todo": [],
+        "token": ""
+      }
     }
-  }
 
+    logout() {
+      this.set_token('')
+    }
 
-  componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/users/').then(response =>{
+    is_auth() {
+      return !!this.state.token
+    }
+    set_token(token) {
+      const cookies = new Cookies()
+      cookies.set('token', token)
+      console.log(token + "<-----------------------------pl");
+      this.setState({
+        'token': token
+      }, () => this.load_data())
+    }
+    get_token_from_storage() {
+      const cookies = new Cookies()
+      const token = cookies.get('token')
+      this.setState({
+        'token': token
+      }, () => this.load_data())
+    }
 
-       this.setState(
-        {
+    get_token(username, password) {
+      const data = {
+        username: username,
+        password: password
+      }
+      axios.post('http://127.0.0.1:8000/api-token-auth/', data).then(response => {
+        this.set_token(response.data['token'])
+      }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+    load_data() {
+      const headers = this.get_headers()
+      axios.get('http://127.0.0.1:8000/api/users/', {headers}).then(response => {
+
+        this.setState({
           'users': response.data.results
-        }
-    )
-    }).catch(error => console.log(error))
-    axios.get('http://127.0.0.1:8000/api/projects/').then(response =>{
+        })
+      }).catch(error => console.log(error))
+      axios.get('http://127.0.0.1:8000/api/projects/', {headers}).then(response => {
 
-       this.setState(
-        {
+        this.setState({
           'projects': response.data.results
-        }
-    )
-    }).catch(error => console.log(error))
-    axios.get('http://127.0.0.1:8000/api/notes/').then(response =>{
+        })
+      }).catch(error => console.log(error))
+      axios.get('http://127.0.0.1:8000/api/notes/', {headers}).then(response => {
 
-       this.setState(
-        {
+        this.setState({
           'todo': response.data.results
-        }
-    )
-    }).catch(error => console.log(error))
-    // const users = [
-    //   {
-    //     'first_name':'Фёдор',
-    //     'last_name':'Достаевский',
-    //     'birthday_year':1821,
-    //   },
-    //   {
-    //     'first_name':'Александр',
-    //     'last_name':'Грин',
-    //     'birthday_year':1880,
-    //   }
-    //
-    // ]
-    // this.setState({
-    //   "users": users
-    // })
-  }
+        })
+      }).catch(error => console.log(error))
+    }
 
+    get_headers(){
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_auth()){
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        console.log(this.is_auth() + "</--------------------------------");
+        return headers
+
+    }
+    componentDidMount() {
+        this.get_token_from_storage()
+    }
 
   render() {
     return (
       <div>
          <BrowserRouter>
            <Header/>
+           <li>
+             {this.is_auth() ? <button onClick={()=> this.logout()}> Logout </button> : <Link to='/login'>Login</Link>}
+           </li>
            <Routes>
                <Route exact path='/' element={<UserList users={this.state.users}/>} />
                <Route exact path='/products' element={<ProjectList projects={this.state.projects}/>}/>
                <Route exact path='/todo' element={<TodoList notes={this.state.todo}/>}/>
+               <Route exact path='/login' element={<LoginForm get_token={(username, password) => this.get_token(username, password)}/>}/>
                <Route path='*' element={<NotFound404 />} />
            </Routes>
            <Footer/>
