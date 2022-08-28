15a183802ab69e3e8d97a812f68c24cca035e8a6
sergey <oborok05@bk.ru>
2022-08-28 19:28:00 +0300

    lesson8-commit

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,3 +1,50 @@
+from django import db
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from authapp.models import User
+
+from todoapp.views import NotesModelViewSet, ProjectsModelViewSet
+from todoapp.models import Note, Project
+
+
+class TestTodoViewSet(TestCase):
+
+    def setUp(self) -> None:
+        self.url = '/api/notes/'
+
+    def test_factory_get_todo(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        view = NotesModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_client_detail(self):
+        client = APIClient()
+        response = client.get(f'{self.url}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestProject(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = '/api/projects/'
+
+    def test_get(self):
+        response = self.client.get(f'{self.url}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_mixer(self):
+        bio = mixer.blend(Note)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def tearDown(self) -> None:
+        pass
